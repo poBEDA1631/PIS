7e9e5d2115c271f1526841b00fa75716af2df089
Delulu <starikovv_kiril@gmail.com>
2026-09-18 13:46:41 +0300

    feat: додати базову модель примірника книги та статуси

---
+++ src/catalog_model.js
@@ -0,0 +1,22 @@
+﻿/**
+ * Базові статуси примірників книги згідно зі специфікацією spec/system_concept.md
+ */
+const CopyStatus = Object.freeze({
+  AVAILABLE: 'AVAILABLE',
+  BORROWED: 'BORROWED',
+  RESERVED: 'RESERVED',
+  LOST: 'LOST',
+});
+
+class BookCopy {
+  constructor(inventoryNumber, status = CopyStatus.AVAILABLE) {
+    this.inventoryNumber = inventoryNumber;
+    this.status = status;
+  }
+
+  isAvailable() {
+    return this.status === CopyStatus.AVAILABLE;
+  }
+}
+
+module.exports = { CopyStatus, BookCopy };
